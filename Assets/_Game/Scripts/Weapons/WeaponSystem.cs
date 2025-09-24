e672536395f6406706db35c1c6e61b9308462cb9
Danila <bezrukov.2002@bk.ru>
2025-09-24 17:20:44 +0300

    start made Weapon System

---
+++ Assets/_Game/Scripts/Weapons/WeaponSystem.cs
@@ -0,0 +1,27 @@
+﻿using Game.Services.Input;
+using Game.Utilities;
+using System;
+using UnityEngine;
+
+namespace Assets._Game.Scripts.Weapons
+{
+    public sealed class WeaponSystem : IInitializable, IDisposable
+    {
+        private readonly Transform _weaponSpawnPosition;
+        private readonly IInputService _inputService;
+
+        public WeaponSystem(Transform weaponSpawnPosition, IInputService inputService)
+        {
+            _weaponSpawnPosition = weaponSpawnPosition;
+            _inputService = inputService;
+        }
+
+        public void Initialize()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
