917716116af4dea5ee2a2e5b8bed60748bc17814
Danila <bezrukov.2002@bk.ru>
2025-09-24 15:52:08 +0300

    Update EntryPoint.cs

--- Assets/_Game/Scripts/EntryPoint.cs
+++ Assets/_Game/Scripts/EntryPoint.cs
@@ -1,31 +1,32 @@
+using Game.Services.Input;
+using Game.Utilities;
 using System;
 using System.Collections.Generic;
-using Game.Utilities;
 using UnityEngine;
-using Game;
-using Game.Services.Input;
 
 namespace Game.Services
 {
     public sealed class EntyPoint : MonoBehaviour
     {
         [SerializeField] private Game.Character _character;
-        
+        [SerializeField] private Camera _mainCamera;
+        [SerializeField] private LayerMask _groundLayer;
+
         private HashSet<IInitializable> _initializables;
         private HashSet<IDisposable> _disposables;
         private HashSet<IFixedUpdatable> _fixedUpdatables;
-    
+
         public void Awake()
         {
             _initializables = new();
             _disposables = new();
             _fixedUpdatables = new();
 
-            var inputService = new InputService();
+            var inputService = new InputService(_mainCamera, _groundLayer);
             _initializables.Add(inputService);
             _disposables.Add(inputService);
             _fixedUpdatables.Add(inputService);
-            
+
             _character.Init(inputService);
         }
 
